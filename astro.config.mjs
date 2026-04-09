13083f29cb5a9ed262d697efd3e568450bf7a57e
qiao13822919184-byte <qiao13822919184@gmail.com>
2026-04-09 16:24:52 +0800

    chore: 优化ui,增加谷歌收录元素，增加robottxt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,19 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   // 替换为你真实的 GitHub 用户名
-  site: 'https://qiao13822919184-byte.github.io', 
+  site: 'https://qiao13822919184-byte.github.io',
+
   // 替换为你的仓库名（必须以 / 开头）
-  base: '/test_2', 
+  base: '/test_2',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
 });
